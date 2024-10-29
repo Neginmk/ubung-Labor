1a4633e77d0fcd1e639357f718ff1a1f86045d18
Negin Mohtajkhoda <Moh240754@spengergasse.at>
2024-10-29 12:41:09 +0100

    Initial sharing of project

---
+++ Labor.java
@@ -0,0 +1,121 @@
+
+
+public class Labor
+{
+    private String gebaude;
+    private String raum;
+    private int stock;
+    private boolean beamer;
+    private int anzZimmer;
+    
+    
+    public Labor(String gebaude,String raum, int stock, boolean beamer,
+    int anZzimmer)
+    {
+        setGebaude (gebaude);
+        setRaum (raum);
+        setStock (stock);
+        setBeamer (beamer);
+        setAnzZimmer (anzZimmer);
+    }
+    
+    public Labor(String gebaude, int stock, boolean beamer, int anzZimmer)
+    {
+        setGebaude(gebaude);
+        setRaum ("C.03.27A");
+        setStock (stock);
+        setBeamer (beamer);
+        setAnzZimmer (anzZimmer);
+    
+        
+    }
+    
+    public Labor(String gebaude, int stock, int anzZimmer)
+    {
+        setGebaude(gebaude);
+        setRaum("C.03.27A");
+        setStock(stock);
+        setBeamer(true);
+        setAnzZimmer(anzZimmer);
+
+    }
+    
+    public Labor(int stock)
+    {
+        setGebaude("C");
+        setRaum("C.03.27A");
+        setStock(stock);
+        setBeamer(true);
+        setAnzZimmer(20);
+        
+    }
+    
+    public Labor()
+    {
+        setGebaude("C");
+        setRaum("C.03.27A");
+        setStock(5);
+        setBeamer(true);
+        setAnzZimmer(20);
+    }
+    
+    
+    public void setGebaude(String gebaude)
+    {
+        this.gebaude = gebaude;
+    }
+    
+    public void setRaum (String raum)
+    {
+        this.raum =raum;
+    }
+    
+    public void setStock(int stock)
+    {
+        this.stock = stock;
+    }
+    
+    public void setBeamer(boolean beamer)
+    {
+        this.beamer = beamer;
+    }
+    
+    public void setAnzZimmer(int anzZimmer)
+    {
+        this.anzZimmer = anzZimmer;
+    }
+    
+    public String getGebaude()
+    {
+        return gebaude;
+    }
+    
+    public String getRaum()
+    {
+        return raum;
+    }
+    
+    public int getStock()
+    {
+        return stock;
+    }
+    
+    public boolean getBeamer()
+    {
+        return beamer;
+    }
+    
+    public int getAnzZimmer()
+    {
+        return anzZimmer;
+    }
+    
+    public String getInitialen()
+    {
+        String initielalen;
+        
+        initielalen= raum.substring (5);
+        
+        return initielalen;
+    }
+}
